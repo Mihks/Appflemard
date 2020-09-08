976b89c17102c0d31e7fbc4e9325b9c414b54938
Mihks <34143350+Mihks@users.noreply.github.com>
2020-09-08 15:54:14 +0100

    Create horaire_retour.php

---
+++ horaire_retour.php
@@ -0,0 +1,42 @@
+<?php 
+
+
+include_once('Espace client/fonction.php');
+
+
+
+if (isset($_POST['trajet']) && isset($_POST['agence']) && isset($_POST['date']) ) {
+	# code...
+	
+$portion =  explode('-', $_POST["trajet"]);
+
+$trajet = $portion[1].'-'.$portion[0];
+
+
+$requete = $bdd->prepare(" SELECT horaire FROM voyage WHERE nom_agence = ? AND nom_trajet = ? AND date_voyage = ? ");
+
+
+$requete->execute(array($_POST["agence"],$trajet,$_POST['date']));
+
+
+echo '<optgroup label="'.$_POST["agence"].'" >';
+
+
+	while ($donnes = $requete->fetch()) {
+  	
+  	
+
+  		echo '<option  value="'.$donnes['horaire'].'">'.$donnes['horaire'].'</option>';
+  	
+  		
+  }  
+
+echo "</optgroup>";
+
+$requete->closeCursor();
+
+
+
+}else
+	echo "erreur...";
+
