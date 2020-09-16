9efb01f900f487f5498dd8c10492c95196ba2cea
Mihks <34143350+Mihks@users.noreply.github.com>
2020-09-16 15:29:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -227,7 +227,8 @@ session_start();
 							
 							<div >
 
-								<input type="submit"  id="envoyer" value="envoyer" style="border-radius: 15px;width: 45px;height: 40px;font-size: 14px;border-bottom: 1px solid orange;cursor: pointer;margin-left: 260px;"  />
+			
+								<button style="border-radius: 15px;width: 45px;height: 40px;font-size: 14px;border-bottom: 1px solid orange;cursor: pointer;"  type="submit" id="envoyer" value="envoyer" >Valider</button>
 
 								<!-- <button class="btn btn-primary" type="submit"><span class="glyphicon glyphicon-ok-sign"></span> Envoyer</button> -->
 
