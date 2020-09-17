5db06c2a3cdb40bb1f7baa194188b8bed8ad4659
Mihks <34143350+Mihks@users.noreply.github.com>
2020-09-17 15:09:17 +0100

    Update hitu.php

--- agence/includes/hitu.php
+++ agence/includes/hitu.php
@@ -2,7 +2,7 @@
 
 								<div>
 									<label for="lieu_depart">Trajet</label>
-									<select name="lieu_depart" id="lieu_depart" class="destination a1">
+									<select name="trajet" id="lieu_depart" class="destination a1">
 										<option value="Libreville-Mouila">Libreville-Mouila</option>
 										<option value="Mouila-Libreville">Mouila-Libreville</option>
 										<option value="Libreville-Fougamou">Libreville-Fougamou</option>
