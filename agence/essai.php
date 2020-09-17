b901f33faf62f14534898027dd49d4fdd029937f
Mihks <34143350+Mihks@users.noreply.github.com>
2020-09-17 17:06:49 +0100

    Update essai.php

--- agence/essai.php
+++ agence/essai.php
@@ -18,19 +18,32 @@ $heure_depart = $_POST['hora'];
  if (isset($_SESSION['agence']) && !empty($_SESSION['agence']) && isset($trajet) && !empty($trajet) && isset($date_depart) && !empty($date_depart) && isset($heure_depart) && !empty($heure_depart)) {
  	
  
-
 	
+	$reponse = $bdd->prepare(" SELECT nombre_place_dispo FROM voyage WHERE nom_agence = ? AND date_voyage = ? AND nom_trajet = ? AND horaire = ?  ");
 
- 	$rep = $bdd->prepare("SELECT place_dispo(:date_voyage,:trajet,:nom_agence,:heure) ");
+	$reponse->execute(array($_POST["agence"],$date_depart,$trajet,$heure_depart));
 
-	$rep->execute(array('date_voyage' => $date_depart,'trajet' => $trajet,'nom_agence' => $_SESSION['agence'],'heure' => $heure_depart));
+	$donnees = $reponse->fetch();
+
+	$place_aller_dispo = $donnees['nombre_place_dispo'];
+
+	$reponse->closeCursor();
+	 
+	 
+	 echo $place_aller_dispo;
+
+	 //////////////////////////
+	 
+//  	$rep = $bdd->prepare("SELECT place_dispo(:date_voyage,:trajet,:nom_agence,:heure) ");
+
+// 	$rep->execute(array('date_voyage' => $date_depart,'trajet' => $trajet,'nom_agence' => $_SESSION['agence'],'heure' => $heure_depart));
 
 
-	$donnee = $rep->fetch();
+// 	$donnee = $rep->fetch();
 
-	echo $donnee[0];
+// 	echo $donnee[0];
 
-	$rep->closeCursor();
+// 	$rep->closeCursor();
 
 
 }else{
