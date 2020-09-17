ced43addce49970c959a2edb22e45f0a94461085
Mihks <34143350+Mihks@users.noreply.github.com>
2020-09-17 15:08:08 +0100

    Update major.php

--- agence/includes/major.php
+++ agence/includes/major.php
@@ -2,7 +2,7 @@
 							<div class="cache" id="destination_major">
 								<div>
 									<label for="lieu_dep_maj">Trajet</label>
-									<select name="lieu_dep_maj" id="lieu_dep_maj" class="destination a1">
+									<select name="trajet" id="lieu_dep_maj" class="destination a1">
 										<option value="Libreville-Oyem" id="lbv">Libreville-Oyem</option>
 										<option value="Oyem-Libreville" >Oyem-Libreville</option>
 										<option value="Libreville-Bitam">Libreville-Bitam</option>
