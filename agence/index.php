3f93ee5a70a946548d14990a5fbb88bacbb11d30
Mihks <34143350+Mihks@users.noreply.github.com>
2020-09-17 16:58:16 +0100

    Update index.php

--- agence/index.php
+++ agence/index.php
@@ -1701,8 +1701,11 @@ button{
 	</div>
 
 		
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.1/jquery.min.js"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<!-- 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.1/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script> -->
+	
+	<script src="js/jquery.min.js"></script>
+	<script src="js/jquery-ui.min.js"></script>
 
 	<script type="text/javascript" src="js/filereader.js"></script>
 	        <!-- Using jquery version: -->
