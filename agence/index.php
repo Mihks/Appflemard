d9854b6f55a7ac6899b8675a769df7fa99d68de5
Mihks <34143350+Mihks@users.noreply.github.com>
2020-09-17 17:56:14 +0100

    Update index.php

--- agence/index.php
+++ agence/index.php
@@ -545,7 +545,7 @@ button{
  		</div> -->
 	</header>
 
-	<br><br><br><br><br><br><br><br>
+	<br><br><br><br><br><br><br><br><br><br>
 
 		
 	<section >
@@ -1701,8 +1701,8 @@ button{
 	</div>
 
 		
-<!-- 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.1/jquery.min.js"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script> -->
+<!-- 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.1/jquery.min.js"></script> -->
+<!--	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script> -->
 	
 	<script src="js/jquery.min.js"></script>
 	<script src="js/jquery-ui.min.js"></script>
