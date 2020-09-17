437597d3971db2ae1716d96b1eee1293cbf72fb2
Mihks <34143350+Mihks@users.noreply.github.com>
2020-09-17 15:14:53 +0100

    Add files via upload

---
+++ agence/includes/transporter.php
@@ -0,0 +1,16 @@
+							<div class="cache" id="destination_transp">
+								<div >
+									<label for="lieu_dep_transp">Trajet</label>
+									<select name="trajet" id="lieu_dep_transp" class="destination a1" >
+										<option value="Libreville-Oyem" id="lbv">Libreville-Oyem</option>
+										<option value="Oyem-Libreville" >Oyem-Libreville</option>
+										<option value="Libreville-Bitam">Libreville-Bitam</option>
+										<option value="Bitam-Libreville">Bitam-Libreville</option>
+										<option value="Libreville-Mitzic">Libreville-Mitzic</option>
+										<option value="Mitzic-Libreville">Mitzic-Libreville</option>
+
+									</select>
+								</div>	
+
+	
+							</div>
