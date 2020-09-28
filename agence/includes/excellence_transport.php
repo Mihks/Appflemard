abdf168115611b9f4f60fcf661962d3be26ede1b
Mihks <34143350+Mihks@users.noreply.github.com>
2020-09-28 01:07:43 +0100

    Update excellence_transport.php

--- agence/includes/excellence_transport.php
+++ agence/includes/excellence_transport.php
@@ -2,8 +2,6 @@
 								<div >
 									<label for="lieu_dep_excel">Trajet</label>
 									<select name="trajet" id="lieu_dep_excel" class="destination a1" >
-										<option value="Libreville-Mouila">Libreville-Mouila</option>
-										<option value="Mouila-Libreville" >Mouila-Libreville</option>
 										<option value="Libreville-Gamba">Libreville-Gamba</option>
 										<option value="Gamba-Libreville">Gamba-Libreville</option>		
 									</select>
