f6e913b742652a52163c5000b260cf0d16775f88
Mihks <34143350+Mihks@users.noreply.github.com>
2020-09-30 17:49:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -204,7 +204,7 @@ session_start();
 
 
 							
-							<div style='border: 1px solid black;margin-left: 30px;' id='valid' >
+							<div style='margin-left: 30px;' id='valid' >
 
 								<button style="border-radius: 15px;width: 100px;height: 40px;font-size: 14px;border-bottom: 1px solid orange;cursor: pointer;"  type="submit" id="envoyer" value="envoyer" >Valider</button>
 
