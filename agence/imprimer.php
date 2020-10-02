cf70239105b1f6a417a11c243a4730b9480be62b
Mihks <34143350+Mihks@users.noreply.github.com>
2020-10-02 18:06:09 +0100

    Update imprimer.php

--- agence/imprimer.php
+++ agence/imprimer.php
@@ -1,6 +1,6 @@
 <?php
 
-$ch = curl_init('https://webtopdf.expeditedaddons.com/?api_key=' . getenv('WEBTOPDF_API_KEY') . '&content=http%3A%2F%2Fwww.wikipedia.org&html_width=1024&margin=10&title=My+PDF+Title');
+$ch = curl_init('https://webtopdf.expeditedaddons.com/?api_key='.getenv('WEBTOPDF_API_KEY').'&content=http%3A%2F%2Fwww.wikipedia.org&html_width=1024&margin=10&title=My+PDF+Title');
 
 $response = curl_exec($ch);
 curl_close($ch);
