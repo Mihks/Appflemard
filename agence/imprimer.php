842b748034526ada8d1c2163f078caf0c5fa0574
Mihks <34143350+Mihks@users.noreply.github.com>
2020-10-02 16:26:59 +0100

    Update imprimer.php

--- agence/imprimer.php
+++ agence/imprimer.php
@@ -1,8 +1,8 @@
 <?php
 
-session_name('flemadmin');
+// session_name('flemadmin');
 
-session_start();
+// session_start();
 
 // require('fpdf/fpdf.php');
 
@@ -394,7 +394,7 @@ session_start();
 // var_dump($response);
 
 
-$ch = curl_init('https://webtopdf.expeditedaddons.com/?api_key='.getenv('WEBTOPDF_API_KEY').'&content=http%3A%2F%2Fwww.wikipedia.org&html_width=1024&margin=10&title=My+PDF+Title');
+$ch = curl_init('https://webtopdf.expeditedaddons.com/?api_key='.getenv('WEBTOPDF_API_KEY').'&content=https://www.wikipedia.org&html_width=1024&margin=10&title=My+PDF+Title');
 
 $response = curl_exec($ch);
 curl_close($ch);
