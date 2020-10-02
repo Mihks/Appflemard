94844a4edacd01fc4fc8f6065c9171d9e993c5de
Mihks <34143350+Mihks@users.noreply.github.com>
2020-10-02 18:25:03 +0100

    Update imprimer.php

--- agence/imprimer.php
+++ agence/imprimer.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 $src = 'https://www.flemard.ga';
-$ch = curl_init('https://webtopdf.expeditedaddons.com?api_key='.getenv('WEBTOPDF_API_KEY').'&content='.$src.'&html_width=1024&margin=10&title=klein_mihks');
+$ch = curl_init('https://webtopdf.expeditedaddons.com?api_key='.getenv('WEBTOPDF_API_KEY').'&content='.$src.'&html_width=1024px&margin=10&title=klein_mihks');
 $response = curl_exec($ch);
 curl_close($ch);
 var_dump($response);
