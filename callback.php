7d5b4b24ffc17c2a4100bc86d7164435c63537e6
Mihks <34143350+Mihks@users.noreply.github.com>
2020-10-02 23:52:55 +0100

    Update callback.php

--- callback.php
+++ callback.php
@@ -17,10 +17,6 @@ $tel = '074'.mt_rand(200040,241743);
 
  $statut = ($code_statut==200) ? "Succes" : "Echoue" ;
 
-$requete = $bdd->prepare('UPDATE transaction SET statut = ?  WHERE ref_trans = ? ');
-									
-$requete->execute(array($statut,$_SESSION['ref_trans']));
-
 
 $requete = $bdd->prepare('UPDATE paiement SET code_statut = ? WHERE ref_trans = ? ');
 									
@@ -31,5 +27,9 @@ $requete = $bdd->prepare('UPDATE client SET tel_client = ?  WHERE id_client = ? 
 
 $requete->execute(array($tel,$_SESSION['id_client']));
 
+
+$requete = $bdd->prepare('UPDATE transaction SET statut = ?  WHERE ref_trans = ? ');
+									
+$requete->execute(array($statut,$_SESSION['ref_trans']));
 
 header("Location: resultat_transaction.php");
