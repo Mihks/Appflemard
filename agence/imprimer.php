056b0737c79fe3cbb213678d9f35afd98d788821
Mihks <34143350+Mihks@users.noreply.github.com>
2020-10-02 15:58:11 +0100

    Update imprimer.php

--- agence/imprimer.php
+++ agence/imprimer.php
@@ -404,5 +404,5 @@ $pvitform = '<form id="pvitform" method="POST" action="https://webtopdf.expedite
 	<script type="text/javascript">
 		document.getElementById("pvitform").onload();
 	</script>';
-	echo($pvitform);
+	vardump($pvitform);
 ?>
