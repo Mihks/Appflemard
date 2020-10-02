ed8c4ea20fa567300546525c74782ea0b3045121
Mihks <34143350+Mihks@users.noreply.github.com>
2020-10-02 19:42:49 +0100

    Update traitements.php

--- traitements.php
+++ traitements.php
@@ -369,7 +369,7 @@ if ( ( preg_match("#Aller_retour#", $_POST['type_billet']) && $place_aller_dispo
 		$_SESSION['id_client'] = $id_client;
 
 
-
+		header('Location: callback.php');
 
 ///////////////////////////////////////////////////fin insertion/////////////////////////////////////////:
 //envoies les infos API mypivit
