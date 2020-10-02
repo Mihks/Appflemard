b05f176a149b782747105f48caa744d862cc8ace
Mihks <34143350+Mihks@users.noreply.github.com>
2020-10-02 17:41:26 +0100

    Update and rename imprimer.php to imprimer.rjs

--- agence/imprimer.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$ch = curl_init('https://webtopdf.expeditedaddons.com/?api_key='.getenv('WEBTOPDF_API_KEY').'&content=http%3A%2F%2Fwww.wikipedia.org&html_width=1024&margin=10&title=My+PDF+Title');
-
-$response = curl_exec($ch);
-curl_close($ch);
-
-var_dump($response);
