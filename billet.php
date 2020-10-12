c7cf5da2fc88111ee80ab2ddd06b0386c02a7a57
Mihks <34143350+Mihks@users.noreply.github.com>
2020-10-12 22:33:16 +0100

    Update billet.php

--- billet.php
+++ billet.php
@@ -1,5 +1,5 @@
 <?php
-header ("Content-type: image/png"); // 1 : on indique qu'on va envoyer une image PNG
+header("Content-type: image/png"); // 1 : on indique qu'on va envoyer une image PNG
 $image = imagecreate(200,50); // 2 : on crée une nouvelle image de taille 200 x 50
 // 3 : on s'amuse avec notre image (on va apprendre à le faire)
 imagepng($image); // 4 : on a fini de faire joujou, on demande à afficher l'image
