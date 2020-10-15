ce864f402b46b448d310e61e8864d2dff4b0d1a9
Mihks <34143350+Mihks@users.noreply.github.com>
2020-10-15 01:19:53 +0100

    Update callback.php

--- callback.php
+++ callback.php
@@ -4,21 +4,32 @@ session_name("flemard");
 
 session_start();
 
+include_once 'fonction.php';
 
-$data_received=file_get_contents("php://input");
-$data_received_xml=new SimpleXMLElement($data_received);
-$ligne_response=$data_received_xml[0];
+$data_received = file_get_contents("php://input");
+
+$data_received_xml = new SimpleXMLElement($data_received);
+
+$ligne_response = $data_received_xml[0];
+
 $interface_received=$ligne_response->INTERFACEID;
+
 $reference_received=$ligne_response->REF;
+
 $type_received=$ligne_response->TYPE;
+
 $statut_received=$ligne_response->STATUT;
+
 $operateur_received=$ligne_response->OPERATEUR;
+
 $client_received=$ligne_response->TEL_CLIENT;
+
 $message_received=$ligne_response->MESSAGE;
+
 $token_received=$ligne_response->TOKEN;
+
 $agent_received=$ligne_response->AGENT;
 
-include_once 'fonction.php';
 
 $_SESSION['code_statut'] = $statut_received;
 
