0b464405765167261c7a00363fe86c5fb55776a8
Mihks <34143350+Mihks@users.noreply.github.com>
2020-10-20 11:23:36 +0100

    Update callback.php

--- callback.php
+++ callback.php
@@ -1,110 +1,33 @@
 <?php
 
-session_name("flemard");
-
-session_start();
-
-include_once 'fonction.php';
-
 $data_received = file_get_contents("php://input");
 
-$data_received_xml = new SimpleXMLElement($data_received);
+// $data_received_xml = new SimpleXMLElement($data_received);
 
-$ligne_response = $data_received_xml[0];
+$data = explode('&',$data_received);
 
-$interface_received=$ligne_response->INTERFACEID;
+$ref_received = explode('=',$data[0]);
+$reference_received = $ref_received[1];
 
-$reference_received=$ligne_response->REF;
+$stat_received = explode('=',$data[1]);
+$statut_received = $stat_received[1];
 
-$type_received=$ligne_response->TYPE;
+// $ligne_response = $data_received_xml[0];
 
-$statut_received=$ligne_response->STATUT;
+// $interface_received=$ligne_response->INTERFACEID;
 
-$operateur_received=$ligne_response->OPERATEUR;
+// $reference_received=$ligne_response->REF;
 
-$client_received=$ligne_response->TEL_CLIENT;
+// $type_received=$ligne_response->TYPE;
 
-$message_received=$ligne_response->MESSAGE;
+// $statut_received=$ligne_response->STATUT;
 
-$token_received=$ligne_response->TOKEN;
+// $operateur_received=$ligne_response->OPERATEUR;
 
-$agent_received=$ligne_response->AGENT;
+// $client_received=$ligne_response->TEL_CLIENT;
 
+// $message_received=$ligne_response->MESSAGE;
 
-$_SESSION['code_statut'] = $statut_received;
+// $token_received=$ligne_response->TOKEN;
 
-//$tel = '074'.mt_rand(200040,241743);
-
-
-$statut = ($code_statut==200) ? "Succes" : "Echoue" ;
-
-
-$requete = $bdd->prepare('UPDATE paiement SET code_statut = ? WHERE ref_trans = ? ');
-									
-$requete->execute(array($statut_received,$reference_received));
-
-
-$requete = $bdd->prepare('UPDATE transaction SET statut = ?  WHERE ref_trans = ? ');
-									
-$requete->execute(array($statut,$reference_received));
-
-
-$requete = $bdd->prepare('UPDATE client SET tel_client = ?  WHERE id_client = ? ');
-
-$requete->execute(array($client_received,$_SESSION['id_client']));
-
-
-///si le $statut est égal à succes
-if($_SESSION['code_statut']==200){
-
-$requete = $bdd->prepare('UPDATE
-     reservation
-   SET etat_voyage_1 = "Valide", etat_voyage_2 = "Valide"
-    WHERE
-        id_reservation = ? ');
-
-$requete->execute(array($_SESSION['id_reservation']));
-
-
-$requete = $bdd->prepare('UPDATE
-     voyage
- 	SET nombre_place_dispo = ?, nombre_place_reserve = ? ,nombre_place = ?
-    WHERE
-        date_voyage = ? AND horaire = ? AND nom_trajet = ? AND nom_agence = ? ');
-
-$requete->execute(array($_SESSION['new_place_dispo'],$_SESSION['new_place_reserve'],$_SESSION['new_nombre_place'],$_SESSION['date_voyage'],$_SESSION['horaire'],$_SESSION['trajet'],$_SESSION['_agence']));
-
-
-if ($_SESSION['type'] =='Aller_retour'){
-
-$requete = $bdd->prepare('UPDATE
-     voyage
- 	SET nombre_place_dispo = ?, nombre_place_reserve = ? ,nombre_place = ?
-    WHERE
-        date_voyage = ? AND horaire = ? AND nom_trajet = ? AND nom_agence = ? ');
-
-$requete->execute(array($_SESSION['new_place_dispo_retour'],$_SESSION['new_place_reserve_retour'],$_SESSION['new_nombre_place_retour'],$_SESSION['date_voyage_retour'],$_SESSION['horaire_retour'],$_SESSION['trajet_retour'],$_SESSION['_agence']));
-
-	}
-
-}
-////////////////////////////////
-
-
-/////si $statut est diff de succes
-if($_SESSION['code_statut']!=200){
-
-$requete = $bdd->prepare('UPDATE
-     reservation
-   SET etat_voyage_1 = "Avorte", etat_voyage_2 = "Avorte"
-    WHERE
-        id_reservation = ? ');
-$requete->execute(array($_SESSION['id_reservation']));
-
-}
-///////////////////////////////////////////
-
-
-///////////////////////////////////////
-
- header("Location: resultat_transaction.php");
+// $agent_received=$ligne_response->AGENT;
