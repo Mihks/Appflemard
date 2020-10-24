6d828abfa982801594f4e58a0a99e1bc5f240ddc
Mihks <34143350+Mihks@users.noreply.github.com>
2020-10-24 16:13:22 +0100

    Update kylian.php

--- kylian.php
+++ kylian.php
@@ -4,15 +4,29 @@
  -->
 <?php
 
+include_once('fonction.php');
 
-echo(phpinfo());
-// include_once('fonction.php');
+$reponse = $bdd->query(" SELECT token FROM compte_marchand LIMIT 1 ");		
+$donnees = $reponse->fetch();
+$token = $donnees['token'];
+$ref = uniqid();
 
-// $ref = uniqid();
+$pvitCurl = '$ch = curl_init();
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_URL,"https://mypvit.com/pvit-secure-full-api.kk");
+curl_setopt($ch, CURLOPT_POSTFIELDS,
+"tel_marchand=077565805
+&montant=100
+&ref='.$ref.'&tel_client=074872120
+&token='.$token.'&action=2
+&service=REST
+&operateur=AM
+&agent=caisse3");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$resultat = curl_exec($ch)';
 
-// $reponse = $bdd->query(" SELECT token FROM compte_marchand LIMIT 1 ");
-		
-// $donnees = $reponse->fetch();
+
+// echo(phpinfo());
 
 // echo('<!DOCTYPE html>
 
