d87442e041b81735bef7f28a00b13ba1ad1a1f21
Mihks <34143350+Mihks@users.noreply.github.com>
2020-10-24 15:52:51 +0100

    Update kylian.php

--- kylian.php
+++ kylian.php
@@ -3,60 +3,63 @@
 // 	<label>Gain Total</label><input type="text" value="400000" />
  -->
 <?php
-include_once('fonction.php');
 
-$ref = uniqid();
 
-$reponse = $bdd->query(" SELECT token FROM compte_marchand LIMIT 1 ");
+echo(phpinfo);
+// include_once('fonction.php');
+
+// $ref = uniqid();
+
+// $reponse = $bdd->query(" SELECT token FROM compte_marchand LIMIT 1 ");
 		
-$donnees = $reponse->fetch();
+// $donnees = $reponse->fetch();
 
-echo('<!DOCTYPE html>
+// echo('<!DOCTYPE html>
 
-<html>
-<head>
-	<title>Redistribution</title>
-</head>
-<body>
+// <html>
+// <head>
+// 	<title>Redistribution</title>
+// </head>
+// <body>
 
 
-<div>
-	<h1>Redistribution</h1>
-</div>
+// <div>
+// 	<h1>Redistribution</h1>
+// </div>
 
-<div>
+// <div>
 
-<form id="pvitform" method="POST" action="https://mypvit.com/pvit-secure-full-api.kk" >
+// <form id="pvitform" method="POST" action="https://mypvit.com/pvit-secure-full-api.kk" >
 
-Tel Marchand
-<select name="tel_marchand" >
- 		<option value="077565805">Airtel Money</option>
- 		<option value="062691284">Mobi Cash</option>
- 	</select>
+// Tel Marchand
+// <select name="tel_marchand" >
+//  		<option value="077565805">Airtel Money</option>
+//  		<option value="062691284">Mobi Cash</option>
+//  	</select>
 	
-	Montant
-	<input type="text" name="montant" value="100" />	
+// 	Montant
+// 	<input type="text" name="montant" value="100" />	
 	
-	<input type="hidden" name="ref" value="'.$ref.'" />
+// 	<input type="hidden" name="ref" value="'.$ref.'" />
 	
-	<input type="hidden" name="action" value="2" />
+// 	<input type="hidden" name="action" value="2" />
 	
-	<input type="hidden" name="service" value="REST" />
+// 	<input type="hidden" name="service" value="REST" />
 	
-	Agence
-	<select name="tel_client" >
- 		<option value="074872120">Mihky</option>
- 	</select>
+// 	Agence
+// 	<select name="tel_client" >
+//  		<option value="074872120">Mihky</option>
+//  	</select>
 	
-	Operateur
-	<select>
-		<option value="AM">Airtel Money</option>
- 		<option value="MC">Mobi Cash</option>
- 	</select>
+// 	Operateur
+// 	<select>
+// 		<option value="AM">Airtel Money</option>
+//  		<option value="MC">Mobi Cash</option>
+//  	</select>
 
- <input type="hidden" name="token" value="'.$donnees['token'].'" />	
- 	<input type="submit" value="payer" />	
- </form>
-</div></body>
-</html>
-');
+//  <input type="hidden" name="token" value="'.$donnees['token'].'" />	
+//  	<input type="submit" value="payer" />	
+//  </form>
+// </div></body>
+// </html>
+// ');
