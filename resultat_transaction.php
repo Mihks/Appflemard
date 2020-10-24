bbb388af26aa4bbcaa391fded15e7076b5ffe1b7
Mihks <34143350+Mihks@users.noreply.github.com>
2020-10-24 11:22:48 +0100

    Update resultat_transaction.php

--- resultat_transaction.php
+++ resultat_transaction.php
@@ -4,7 +4,6 @@ session_start();
 include_once 'fonction.php';
 include_once 'callback.php';
 
-
 $statut = ($statut_received==200) ? "Succes" : "Echoue";
 
 $requete = $bdd->prepare('UPDATE paiement SET code_statut = ? WHERE ref_trans = ? ');
@@ -20,6 +19,30 @@ $requete->execute(array($statut,$reference_received));
 // $requete->execute(array($client_received,$_SESSION['id_client']));
 
 
+$reponse = $bdd->prepare("SELECT reservation.type_reservation,reservation.nombre_place,
+
+	reservation.id_reservation,reservation.trajet,
+	reservation.date_depart,reservation.heure_depart,
+	reservation.trajet_retour,reservation.date_retour,reservation.heure_retour,nom_agence 
+	FROM reservation,transaction 
+	WHERE reservation.id_reservation = transaction.id_reservation
+	AND reservation.ref_trans = ? ") :
+$reponse->execute(array($reference_received));
+
+$donnees = $reponse->fetch();
+
+$reponse = $bdd->prepare(" SELECT nombre_place_dispo, nombre_place_reserve FROM voyage WHERE nom_agence = ? AND date_voyage = ? AND nom_trajet = ? AND horaire = ?  ");
+
+$reponse->execute(array($donnees['nom_agence'],$donnees['date_depart'],$donnees['trajet'],$donnees['heure_depart']));
+
+$donnee = $reponse->fetch();
+
+$new_place_dispo = $donnee['nombre_place_dispo'] - $donnees['nombre_place'] ;
+	
+$new_place_reserve = $donnee['nombre_place_reserve'] + $donnees['nombre_place'] ;
+	
+
+
 ///si le $statut est égal à succes
 if(isset($statut_received) && !empty($statut_received) && $statut_received==200){
 
@@ -29,27 +52,37 @@ $requete = $bdd->prepare('UPDATE
     WHERE
         id_reservation = ? ');
 
-$requete->execute(array($_SESSION['id_reservation']));
+$requete->execute(array($donnees['id_reservation']));
 
-
+	
 $requete = $bdd->prepare('UPDATE
      voyage
- 	SET nombre_place_dispo = ?, nombre_place_reserve = ? ,nombre_place = ?
+ 	SET nombre_place_dispo = ?, nombre_place_reserve = ? 
     WHERE
         date_voyage = ? AND horaire = ? AND nom_trajet = ? AND nom_agence = ? ');
 
-$requete->execute(array($_SESSION['new_place_dispo'],$_SESSION['new_place_reserve'],$_SESSION['new_nombre_place'],$_SESSION['date_voyage'],$_SESSION['horaire'],$_SESSION['trajet'],$_SESSION['_agence']));
+$requete->execute(array($new_place_dispo,$new_place_reserve,$donnees['date_depart'],$donnees['heure_depart'],$donnees['trajet'],$donnees['nom_agence']));
 
 
-if ($_SESSION['type'] =='Aller_retour'){
+if ($donnees['type_reservation'] =='Aller_retour'){
+
+	$reponse = $bdd->prepare(" SELECT nombre_place_dispo, nombre_place_reserve FROM voyage WHERE nom_agence = ? AND date_voyage = ? AND nom_trajet = ? AND horaire = ?  ");
+
+	$reponse->execute(array($donnees['nom_agence'],$donnees['date_retour'],$donnees['trajet_retour'],$donnees['heure_retour']));
+
+	$donnee = $reponse->fetch();
+
+	$new_place_dispo = $donnee['nombre_place_dispo'] - $donnees['nombre_place'] ;
+	
+	$new_place_reserve = $donnee['nombre_place_reserve'] + $donnees['nombre_place'] ;
 
 $requete = $bdd->prepare('UPDATE
      voyage
- 	SET nombre_place_dispo = ?, nombre_place_reserve = ? ,nombre_place = ?
+ 	SET nombre_place_dispo = ?, nombre_place_reserve = ? 
     WHERE
         date_voyage = ? AND horaire = ? AND nom_trajet = ? AND nom_agence = ? ');
 
-$requete->execute(array($_SESSION['new_place_dispo_retour'],$_SESSION['new_place_reserve_retour'],$_SESSION['new_nombre_place_retour'],$_SESSION['date_voyage_retour'],$_SESSION['horaire_retour'],$_SESSION['trajet_retour'],$_SESSION['_agence']));
+$requete->execute(array($new_place_dispo,$new_place_reserve,$donnees['date_retour'],$donnees['heure_retour'],$donnees['trajet_retour'],$donnees['nom_agence']));
 
 	}
 	
@@ -67,7 +100,7 @@ $requete = $bdd->prepare('UPDATE
    SET etat_voyage_1 = "Avorte", etat_voyage_2 = "Avorte"
     WHERE
         id_reservation = ? ');
-$requete->execute(array($_SESSION['id_reservation']));
+$requete->execute(array($donnees['id_reservation']));
 
 }
 	
