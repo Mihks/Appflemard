e759f7743d92f1781282f8a806f0f8ceeab2c23e
Mihks <34143350+Mihks@users.noreply.github.com>
2020-11-01 23:55:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ include_once 'fonction.php';
 				<li><img src="agence/images/51459340_949759081892876_3562716998855032832_n.jpg" alt="" width="450" height="300" /></li>
 				<li><img src="agence/images/532d5cf1f3749247e6221c63fea18c3b.jpg" alt="" width="450" height="300" /></li>
 				<li><img src="agence//images/unnamed.jpg" alt="" width="450" height="300" /></li>
-				<li><img src="agence/images/6.png" alt="" width="450" height="300" /></li>
+				<li><img src="agence/images/téléchargé.jpg" alt="" width="450" height="300" /></li>
 			</ul>
 			</div>
 		</div>
