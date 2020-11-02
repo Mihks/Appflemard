53b6feadea85400cac051bd9ac2802f49b13d74c
Mihks <34143350+Mihks@users.noreply.github.com>
2020-11-02 09:27:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -58,12 +58,12 @@ include_once 'fonction.php';
 			
 		<div align='center'>
 			<div class="slideshow">
-			<ul>
-				<li><img src="agence/images/317196.jpg" alt="" style='width:75%;height:25%;'/></li>
+<!-- 			<ul> -->
+				<img src="agence/images/317196.jpg" alt="" style='width:60%;height:25%;'/>
 <!-- 				<li><img src="agence/images/itl.cat_bus-wallpaper_2969762.png" alt="" style='width:98.2%;height:500px;' /></li> -->
-!				<li><img src="agence//images/iStock-516654892-e1544537163371.jpg" alt="" style='width:98.2%;height:500px;' /></li>
+<!-- !				<li><img src="agence//images/iStock-516654892-e1544537163371.jpg" alt="" style='width:98.2%;height:500px;' /></li> -->
 <!-- 				<li><img src="agence/images/téléchargé.jpg" alt="" style='width:100%;height:25%;' /></li> -->
-			</ul>
+<!-- 			</ul> -->
 			</div>
 		</div>
 			<h2>Accueil</h2>
