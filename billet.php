3e7ad697c19a20a555dcd97ce75e012f90770a4d
Mihks <34143350+Mihks@users.noreply.github.com>
2020-12-08 03:57:30 +0100

    Update billet.php

--- billet.php
+++ billet.php
@@ -1,51 +1,23 @@
-<?php 
-session_name("flemard");
-session_start();
-include_once('fonction.php');
-include_once('agence/phpqrcode/qrlib.php');
+<?php
 
-header("Content-type: image/jpeg");
+include_once('phpqrcode/qrlib.php');
 
-$reference_received = '5e9ce49ee2034';
+header ("Content-type: image/jpeg");
 
-if(isset($reference_received ) && !empty($reference_received ) ){
-	
-$destination = imagecreatefromjpeg("agence/images/billet.jpg");
-	
-	
-//  $reponse =  $bdd->prepare("SELECT CONCAT(SUBSTRING(UPPER(reservation.nom_agence),1,3),SUBSTRING(reservation.id_reservation,-3),SUBSTRING( client.nom,1,3),SUBSTRING(client.id_client,-3)) AS id,client.nom,CONCAT('+241',client.tel_client) AS num_tel,reservation.type_reservation,reservation.nombre_place,reservation.trajet,DATE_FORMAT(reservation.date_depart, '%W, %e %M %Y') AS depart,reservation.heure_depart,CONCAT(paiement.montant_debite,'FCFA') AS montant,DATE_FORMAT(transaction.date_reservation, '%W %e %M %Y %T') AS date_reserve, reservation.nom_agence,paiement.ref_trans AS ref FROM client,paiement,reservation,transaction WHERE reservation.id_reservation = transaction.id_reservation AND paiement.ref_trans = transaction.ref_trans AND client.id_client = transaction.id_client AND paiement.ref_trans = ?");
+$destination = imagecreatefromjpeg("billet.jpg");
 
-//             $reponse->execute(array($reference_received));
 
-//             $donnees = $reponse->fetch();
+$id = uniqid();
 
-//             $nom_agence = $donnees['nom_agence'];
 
-//             $id = $donnees['id'];
-
-//             $depart = $donnees['depart'];
-
-//             $heure = $donnees['heure_depart'];
-
-//             $trajet = $donnees['trajet'];
-
-//             $ref = $donnees['ref'];
-	
-// 	    $nom = $donnees['nom'];
-
-//             $type = $donnees['type_reservation'];
-
-//             $date_reserve = $donnees['date_reserve'];
-
-//             $reponse->closeCursor();
-
- $id = 'lhgjjf222554';
-	
 QRcode::png($id,'codeqr.png','M',4,2);
 
 $noir = imagecolorallocate($destination, 0, 0, 0);
     
 $red = imagecolorallocate($destination, 128, 0, 0);
+
+            
+
 
 
  if (!file_exists("codeqr.png")){
@@ -64,19 +36,17 @@ $red = imagecolorallocate($destination, 128, 0, 0);
 
   imagestring($destination, 4, 215, 155, '13:30', $noir); // heure
 
-  imagestring($destination, 4, 215, 188,'Libreville-Oyem', $noir); // Trajet
+    imagestring($destination, 4, 215, 188,'Libreville-Oyem', $noir); // Trajet
         
-  imagestring($destination, 4, 245, 221, "45mgkk548", $noir); // ref trans
+    imagestring($destination, 4, 245, 221, "45mgkk548", $noir); // ref trans
 
-  imagestring($destination, 4, 510, 350, 'Aller simple', $noir); // type reservation
+     imagestring($destination, 4, 510, 350, 'Aller simple', $noir); // type reservation
 
-  imagestring($destination, 4, 195, 350, 'Moudiba IV', $noir); // type reservation
+     imagestring($destination, 4, 195, 350, 'Moudiba IV', $noir); // type reservation
 
 
  imagecopymerge($destination,$source, 900, 152, 0, 0, 100, 100, 60); //PERMET DE FUSIONER LES IMAGES
 
+
+
 imagejpeg($destination);
-	
-
-
-}
