347715159748ef78bab4cd4522f75f9f7a42fa44
Mihks <34143350+Mihks@users.noreply.github.com>
2020-12-08 05:37:29 +0100

    Update billet.php

--- billet.php
+++ billet.php
@@ -3,7 +3,7 @@ session_name('flemard');
 session_start();
 // include_once('agence/fpdf/fpdf.php');
 include_once('agence/phpqrcode/qrlib.php');
-include_once('fonction.php');
+
 
 // class PDF extends FPDF
 // {
@@ -60,34 +60,12 @@ include_once('fonction.php');
 // }
 
 $_SESSION['type']='Aller_simple';
-$reference_received ='5e9ce7b0bfde0';
+
     $req = $bdd->exec(" SET lc_time_names = 'fr_FR';");
 
     if (isset($_SESSION['type']) && !empty($_SESSION['type']) && $_SESSION['type']=='Aller_simple' && isset($reference_received) && !empty($reference_received) ) {
 
-        $reponse =  $bdd->prepare("SELECT CONCAT(SUBSTRING(UPPER(reservation.nom_agence),1,3),SUBSTRING(reservation.id_reservation,-3),SUBSTRING( client.nom,1,3),SUBSTRING(client.id_client,-3)) AS id,client.nom,CONCAT('+241',client.tel_client) AS num_tel,reservation.type_reservation,reservation.nombre_place,reservation.trajet,DATE_FORMAT(reservation.date_depart, '%W, %e %M %Y') AS depart,reservation.heure_depart,CONCAT(paiement.montant_debite,'FCFA') AS montant,DATE_FORMAT(transaction.date_reservation, '%W %e %M %Y %T') AS date_reserve, reservation.nom_agence,paiement.ref_trans AS ref FROM client,paiement,reservation,transaction WHERE reservation.id_reservation = transaction.id_reservation AND paiement.ref_trans = transaction.ref_trans AND client.id_client = transaction.id_client AND paiement.ref_trans = ?");
-
-            $reponse->execute(array($reference_received));
-
-            $donnees = $reponse->fetch();
-
-            $nom_agence = $donnees['nom_agence'];
-
-            $id = $donnees['id'];
-
-            $depart = $donnees['depart'];
-
-            $heure = $donnees['heure_depart'];
-
-            $trajet = $donnees['trajet'];
-
-            $ref = $donnees['ref'];
-
-            $type = $donnees['type_reservation'];
-
-            $date_reserve = $donnees['date_reserve'];
-
-            $reponse->closeCursor();
+       include_once('bdd.php');
             
             QRcode::png($id,'codeqr.png','M',4,2);
 
