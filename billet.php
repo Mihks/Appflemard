7f4816bf4d04effcb4d97c5c94264034e1048163
Mihks <34143350+Mihks@users.noreply.github.com>
2020-12-08 03:52:46 +0100

    Update billet.php

--- billet.php
+++ billet.php
@@ -13,34 +13,34 @@ if(isset($reference_received ) && !empty($reference_received ) ){
 $destination = imagecreatefromjpeg("agence/images/billet.jpg");
 	
 	
- $reponse =  $bdd->prepare("SELECT CONCAT(SUBSTRING(UPPER(reservation.nom_agence),1,3),SUBSTRING(reservation.id_reservation,-3),SUBSTRING( client.nom,1,3),SUBSTRING(client.id_client,-3)) AS id,client.nom,CONCAT('+241',client.tel_client) AS num_tel,reservation.type_reservation,reservation.nombre_place,reservation.trajet,DATE_FORMAT(reservation.date_depart, '%W, %e %M %Y') AS depart,reservation.heure_depart,CONCAT(paiement.montant_debite,'FCFA') AS montant,DATE_FORMAT(transaction.date_reservation, '%W %e %M %Y %T') AS date_reserve, reservation.nom_agence,paiement.ref_trans AS ref FROM client,paiement,reservation,transaction WHERE reservation.id_reservation = transaction.id_reservation AND paiement.ref_trans = transaction.ref_trans AND client.id_client = transaction.id_client AND paiement.ref_trans = ?");
+//  $reponse =  $bdd->prepare("SELECT CONCAT(SUBSTRING(UPPER(reservation.nom_agence),1,3),SUBSTRING(reservation.id_reservation,-3),SUBSTRING( client.nom,1,3),SUBSTRING(client.id_client,-3)) AS id,client.nom,CONCAT('+241',client.tel_client) AS num_tel,reservation.type_reservation,reservation.nombre_place,reservation.trajet,DATE_FORMAT(reservation.date_depart, '%W, %e %M %Y') AS depart,reservation.heure_depart,CONCAT(paiement.montant_debite,'FCFA') AS montant,DATE_FORMAT(transaction.date_reservation, '%W %e %M %Y %T') AS date_reserve, reservation.nom_agence,paiement.ref_trans AS ref FROM client,paiement,reservation,transaction WHERE reservation.id_reservation = transaction.id_reservation AND paiement.ref_trans = transaction.ref_trans AND client.id_client = transaction.id_client AND paiement.ref_trans = ?");
 
-            $reponse->execute(array($reference_received));
+//             $reponse->execute(array($reference_received));
 
-            $donnees = $reponse->fetch();
+//             $donnees = $reponse->fetch();
 
-            $nom_agence = $donnees['nom_agence'];
+//             $nom_agence = $donnees['nom_agence'];
 
-            $id = $donnees['id'];
+//             $id = $donnees['id'];
 
-            $depart = $donnees['depart'];
+//             $depart = $donnees['depart'];
 
-            $heure = $donnees['heure_depart'];
+//             $heure = $donnees['heure_depart'];
 
-            $trajet = $donnees['trajet'];
+//             $trajet = $donnees['trajet'];
 
-            $ref = $donnees['ref'];
+//             $ref = $donnees['ref'];
 	
-	    $nom = $donnees['nom'];
+// 	    $nom = $donnees['nom'];
 
-            $type = $donnees['type_reservation'];
+//             $type = $donnees['type_reservation'];
 
-            $date_reserve = $donnees['date_reserve'];
+//             $date_reserve = $donnees['date_reserve'];
 
-            $reponse->closeCursor();
+//             $reponse->closeCursor();
 
-
-
+ $id = 'lhgjjf222554';
+	
 QRcode::png($id,'codeqr.png','M',4,2);
 
 $noir = imagecolorallocate($destination, 0, 0, 0);
